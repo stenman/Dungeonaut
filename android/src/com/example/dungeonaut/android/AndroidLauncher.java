5ef3aed6e990617d394ffb3cb9f637f56f1541aa
Roger Stenman <roger.stenman@gmail.com>
2014-11-26 22:58:57 +0100

    First commit

---
+++ android/src/com/example/dungeonaut/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.example.dungeonaut.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.example.dungeonaut.Dungeonaut;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Dungeonaut(), config);
+	}
+}
---
+++ desktop/src/com/example/dungeonaut/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.example.dungeonaut.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.example.dungeonaut.Dungeonaut;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Dungeonaut(), config);
+	}
+}
